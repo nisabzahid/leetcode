b16f6bc27b9103a7b7c88ef1a706207551737d73
nisab zahid <nz@tsl.io>
2023-12-29 18:11:49 +0600

    7

---
+++ 347. Top K Frequent Elements/347. Top K Frequent Elements.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d = defaultdict(int)
+        for num in nums:
+            key = str(num)
+            if d[key] != None :
+                d[key] = d[key] +1
+            else:
+                d[key]= 1
+        
+        s = sorted(d.items(), key= lambda x:x[1], reverse=True )
+        ans= [ ]
+        # print(d)
+        for key,_ in s:
+            ans.append(int(key))
+            if len(ans) == k:
+                return ans
