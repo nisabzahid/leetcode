c74f5de842637b34b28c3d916263d6bb17665519
nisab zahid <nz@tsl.io>
2024-01-08 10:17:28 +0600

    with set solution

--- 219. Contains Duplicate II/219. Contains Duplicate II.py
+++ 219. Contains Duplicate II/219. Contains Duplicate II.py
@@ -8,3 +8,18 @@ class Solution:
             else:
                d[nums[i]]= max(d[nums[i]] , i) #update the index with bigger index
         return False
+    
+
+
+# faster solution with set
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        if k==0:return False
+        set_=set()
+        for r in range(len(nums)):
+            if nums[r] in set_:return True
+            set_.add(nums[r])
+            if len(set_)==k+1:set_.remove(nums[r-k])
+            #lenth of set is k, if it becomes larger than k ,
+            #then remove the last element from the set
+        return False
